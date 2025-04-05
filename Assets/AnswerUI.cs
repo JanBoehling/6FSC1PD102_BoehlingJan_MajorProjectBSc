cc1ac7f720964bb11971f4188f68ce0c23699554
Jan Boehling <janrasmus03.boehling@t-online.de>
2025-04-05 07:50:54 +0200

    Added ability to select and check answers. The behaviour after checking the answers is still missing

--- Assets/AnswerUI.cs
+++ Assets/AnswerUI.cs
@@ -1,20 +1,59 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class AnswerUI : MonoBehaviour
+public class AnswerUI : MonoBehaviour, IPointerClickHandler
 {
+    public bool IsCorrect { get; private set; }
+    public bool IsSelected { get; private set; }
+    public int Index { get; private set; }
+
     private TMP_Text _answerText;
     private SpriteToTextureConverter _answerImage;
+    private QuizAssignmentController _quizAssignmentController;
+    private Image _backgroundImage;
+    private Color _backgroundBaseColor;
 
     private void Awake()
     {
         _answerText = GetComponentInChildren<TMP_Text>();
         _answerImage = GetComponentInChildren<SpriteToTextureConverter>();
+        _backgroundImage = GetComponent<Image>();
+        _backgroundBaseColor = _backgroundImage.color;
+        _quizAssignmentController = transform.parent.GetComponentInParent<QuizAssignmentController>();
+        if (!_quizAssignmentController) Debug.LogError($"{name}: Error: Could not fetch Quiz Assignment Controller.");
     }
 
-    public void Init(string answerText, Sprite answerSprite)
+    public void Init(string answerText, Sprite answerSprite, bool isCorrect, int index)
     {
         _answerText.text = answerText;
         _answerImage.LoadImage(answerSprite);
+        IsCorrect = isCorrect;
+        Index = index;
+    }
+
+    public void ToggleSelection()
+    {
+        IsSelected = !IsSelected;
+
+        if (IsSelected)
+        {
+            var color = _backgroundImage.color;
+            color.a = .8f;
+            _backgroundImage.color = color;
+
+            transform.localScale = Vector3.one * .8f;
+        }
+        else
+        {
+            _backgroundImage.color = _backgroundBaseColor;
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ToggleSelection();
     }
 }
--- Assets/Scripts/Assignments/Controller/QuizAssignmentController.cs
+++ Assets/Scripts/Assignments/Controller/QuizAssignmentController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -8,7 +10,9 @@ public class QuizAssignmentController : MonoBehaviour
     [SerializeField] private Transform _answerContainer;
     [SerializeField] private QuizAssignment _assignmentData;
 
-    private void OnEnable()
+    private readonly List<AnswerUI> _answerButtons = new();
+
+    private void Start()
     {
         Init(_assignmentData);
     }
@@ -18,12 +22,32 @@ public class QuizAssignmentController : MonoBehaviour
         _questionText.text = quiz.Question;
         _questionImageLoader.LoadImage(quiz.QuestionSprite);
 
-        foreach (var item in quiz.Answers)
+        for (int i = 0; i < quiz.Answers.Length; i++)
         {
-            Debug.Log(item);
+            var answer = quiz.Answers[i];
+
             var answerUIPrefab = Resources.Load<AnswerUI>("AssignmentUI/Answer");
-            var answer = Instantiate(answerUIPrefab, _answerContainer);
-            answer.Init(item.AnswerText, item.AnswerSprite);
+            var answerUI = Instantiate(answerUIPrefab, _answerContainer);
+            answerUI.Init(answer.AnswerText, answer.AnswerSprite, answer.IsCorrect, i);
+            _answerButtons.Add(answerUI);
         }
+    }
+
+    public void CheckAnswer(GameObject container)
+    {
+        var correctAnswers = new List<int>();
+        var selectedAnswers = new List<int>();
+
+        for (int i = 0; i < _answerButtons.Count; i++)
+        {
+            var button = _answerButtons[i];
+
+            if (button.IsCorrect) correctAnswers.Add(button.Index);
+            if (button.IsSelected) selectedAnswers.Add(button.Index);
+        }
+
+        bool isSelectionCorrect = correctAnswers.SequenceEqual(selectedAnswers);
+        if (isSelectionCorrect) Debug.Log("<color=green>Answer correct!");
+        else Debug.Log("<color=red>Answer wrong!");
     }
 }
