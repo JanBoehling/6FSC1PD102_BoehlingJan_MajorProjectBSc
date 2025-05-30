b1234652e7c5988e76bacc07843d82e48dd77270
Jan Boehling <janrasmus03.boehling@t-online.de>
2025-05-30 11:49:36 +0200

    Added database credentials

---
+++ Assets/Scripts/Database/DBCredentials.cs
@@ -0,0 +1,27 @@
+﻿[System.Serializable]
+public struct DBCredentials
+{
+    public string Server;
+    public string UserID;
+    public string Database;
+    public uint Port;
+    public string Password;
+
+    public DBCredentials(string server = "database-5017942646.webspace-host.com", string userID = "dbu3329914", string database = "dbs14278174", uint port = 3306, string password = "")
+    {
+        Server = server;
+        UserID = userID;
+        Database = database;
+        Port = port;
+        Password = password;
+    }
+
+    public string GetConnectionString() => new MySql.Data.MySqlClient.MySqlConnectionStringBuilder()
+    {
+        Server = Server,
+        UserID = UserID,
+        Database = Database,
+        Port = Port,
+        Password = Password
+    }.ConnectionString;
+}
--- Assets/Scripts/Database/DatabaseHandler.cs
+++ Assets/Scripts/Database/DatabaseHandler.cs
@@ -1,26 +1,49 @@
 using MySql.Data.MySqlClient;
+using System.IO;
+using System;
 using Debug = UnityEngine.Debug;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class DatabaseHandler
 {
 #if UNITY_EDITOR
-    private const string ServerAddress = "localhost";
-    private const string User = "root";
-    private const string DatabaseName = "";
-    private const int Port = -1;
-    private const string Password = "";
+    private readonly string CredentialsPath = Path.Combine(Application.dataPath, "DBCredentials.json");
 #else
-    private const string ServerAddress = "";
-    private const string User = "";
-    private const string DatabaseName = "";
-    private const int Port = -1;
-    private const string Password = ""; //I know, this is very dangerous, but I trust you :)
+    private const string CredentialsPath = "";
 #endif
+    private DBCredentials _credentials;
 
     private readonly MySqlConnection _connection;
 
-    public DatabaseHandler() => _connection = new MySqlConnection($"server={ServerAddress};user={User};database={DatabaseName};port={Port};password={Password}");
+    public DatabaseHandler() => _connection = new MySqlConnection(LoadCredentials().GetConnectionString());
     ~DatabaseHandler() => _connection.Close();
+
+    public DBCredentials LoadCredentials()
+    {
+        if (!File.Exists(CredentialsPath))
+        {
+            GeneratePathFile();
+            Debug.Log($"FEHLER: Credentials-Datei fehlt. Öffne DBCredentials.json und fülle die Felder aus. Datei befindet sich im Pfad {CredentialsPath}");
+            return _credentials;
+        }
+
+        using var reader = new StreamReader(CredentialsPath);
+        string json = reader.ReadToEnd();
+
+        _credentials = JsonUtility.FromJson<DBCredentials>(json);
+
+        return _credentials;
+    }
+
+    public void GeneratePathFile()
+    {
+        string json = JsonUtility.ToJson(new DBCredentials(), true);
+        File.Create(CredentialsPath).Close();
+        using var stream = new StreamWriter(CredentialsPath);
+        stream.Write(json);
+        stream.Close();
+    }
 
     /// <summary>
     /// Tries to open up a connection to the database and queries for the given string.
