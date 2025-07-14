fbf4e6d83c6ed0426074c0f3ce93c5d21ff27667
JanBoehling <122728039+JanBoehling@users.noreply.github.com>
2025-07-14 12:40:43 +0200

    Added functionality to display leaderboard

--- Assets/LeaderboardController.cs
+++ Assets/LeaderboardController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -14,8 +15,28 @@ public class LeaderboardController : MonoBehaviour
 
     private void ShowEntries(string[] entriesRaw)
     {
-        (string name, uint XP) entries;
+        // Trim entries to top 10 (10 names, 10 values)
+        System.Array.Resize(ref entriesRaw, 20);
 
-        // for loopen und spitten (jeder ungerader index name, jeder gerade index xp)
+        var entries = new (string name, string XP)[10];
+
+        // Structure raw data into entries
+        int i = 0;
+        for (int j = 0; i < entriesRaw.Length; j += 2)
+        {
+            // No more entries
+            if (string.IsNullOrWhiteSpace(entriesRaw[j])) break;
+
+            entries[i].name = entriesRaw[j];
+            entries[i].XP = entriesRaw[j + 1];
+            i++;
+        }
+
+        // Display entries
+        for (int j = 0; j < entries.Length; j++)
+        {
+            _nameTexts[j].text = entries[j].name;
+            _levelTexts[j].text = entries[j].XP;
+        }
     }
 }
