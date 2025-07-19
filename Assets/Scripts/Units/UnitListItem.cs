6cccaa698c5ddaf0bf70d3de4613cd79e72abcd4
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-19 09:06:29 +0200

    Added icons for the different assignments

--- Assets/Scripts/Units/UnitListItem.cs
+++ Assets/Scripts/Units/UnitListItem.cs
@@ -4,28 +4,42 @@ using UnityEngine.UI;
 
 public class UnitListItem : MonoBehaviour
 {
+    [SerializeField] private Image _buttonBGImage;
     [SerializeField] private Image _iconImage;
     [SerializeField] private TMP_Text _titleText;
-
+    [Space]
     [SerializeField] private Color _baseColor = Color.red;
     [SerializeField] private Color _completedColor = Color.green;
-
-    public void InitUnitListItem(Sprite icon, string title)
-    {
-        _titleText.text = title;
-    }
+    [Space]
+    [SerializeField] private Sprite _videoIcon;
+    [SerializeField] private Sprite _assignmentQAIcon;
 
     public void Init(Milestone milestone)
     {
+        var assignmentData = UnitAndAssignmentManager.Instance.GetAssignmentByID(milestone.Assignments[0]);
+
         _titleText.text = milestone.Title;
+        _iconImage.sprite = assignmentData switch // I love pattern matching. Best feature in C#
+        {
+            VideoAssignment _ => _videoIcon,
+            QAAssignment _ => _assignmentQAIcon,
+            _ => null
+        };
+
+        // Fallback design settings
+        if (_iconImage.sprite == null)
+        {
+            _iconImage.gameObject.SetActive(false);
+            _titleText.rectTransform.offsetMin = new(-_titleText.rectTransform.offsetMax.x, _titleText.rectTransform.offsetMin.y);
+            _titleText.horizontalAlignment = HorizontalAlignmentOptions.Center;
+        }
 
         var button = GetComponent<Button>();
         button.onClick.AddListener(() => RuntimeDataHolder.CurrentMilestone = milestone);
-        //button.onClick.AddListener(() => CompletionTracker.Instance.CurrentMilestone = milestone);
 
-        button.onClick.AddListener(() => UnitAndAssignmentManager.Instance.GetAssignmentByID(milestone.Assignments[0]).LoadAssignmentUI());
+        button.onClick.AddListener(() => assignmentData.LoadAssignmentUI());
 
-        if (milestone.IsCompleted) _iconImage.color = _completedColor;
-        else _iconImage.color = _baseColor;
+        if (milestone.IsCompleted) _buttonBGImage.color = _completedColor;
+        else _buttonBGImage.color = _baseColor;
     }
 }
