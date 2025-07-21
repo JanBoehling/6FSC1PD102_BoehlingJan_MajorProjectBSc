9fe12ecd6057761effabed89dbd5dca1d41e6d2e
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-21 18:40:46 +0200

    Added feedback button to login screen

---
+++ Assets/Scripts/FeedbackWindowController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class FeedbackWindowController : MonoBehaviour
+{
+    [SerializeField] private string _feedbackFormURL;
+
+    private Image _image;
+    private GameObject _container;
+
+    public void Awake()
+    {
+        _image = GetComponent<Image>();
+        _container = transform.GetChild(0).gameObject;
+    }
+
+    public void ToggleVisibility()
+    {
+        _image.enabled = !_image.enabled;
+        _container.SetActive(!_container.activeSelf);
+    }
+
+    public void OpenFeedbackForm()
+    {
+#if !UNITY_EDITOR
+		openWindow(_feedbackFormURL);
+#endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(FeedbackWindowController))]
+public class FeedbackWindowControllerEditor : Editor
+{
+    private FeedbackWindowController _controller;
+
+    private void OnEnable()
+    {
+        _controller = (FeedbackWindowController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (!GUILayout.Button("Toggle Visibility")) return;
+
+        _controller.Awake();
+        _controller.ToggleVisibility();
+    }
+}
+#endif
