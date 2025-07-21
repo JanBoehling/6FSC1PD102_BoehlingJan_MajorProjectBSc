f0772af471f496beebe54b4e6f8801aaf3c020c7
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-21 19:26:49 +0200

    Added functionality to switch sprites of password 'auge' button

--- Assets/Scripts/PasswordVisibilityController.cs
+++ Assets/Scripts/PasswordVisibilityController.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PasswordVisibilityController : MonoBehaviour
 {
@@ -8,15 +9,24 @@ public class PasswordVisibilityController : MonoBehaviour
     [SerializeField] private Sprite _hiddenSprite;
     [SerializeField] private Sprite _visibleSprite;
 
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
     public void TogglePasswordVisibility()
     {
         if (_inputField.contentType == TMP_InputField.ContentType.Password)
         {
             _inputField.contentType = TMP_InputField.ContentType.Standard;
+            if (_visibleSprite) _image.sprite = _visibleSprite;
         }
         else
         {
             _inputField.contentType = TMP_InputField.ContentType.Password;
+            if (_hiddenSprite) _image.sprite = _hiddenSprite;
         }
 
         _inputField.ForceLabelUpdate();
