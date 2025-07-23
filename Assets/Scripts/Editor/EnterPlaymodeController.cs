a070ceab42bd68d7f2a009211f4f83a35320d5ac
JanBoehling <122728039+JanBoehling@users.noreply.github.com>
2025-07-23 21:06:19 +0200

    Added auto login to play mode startup script

--- Assets/Scripts/Editor/EnterPlaymodeController.cs
+++ Assets/Scripts/Editor/EnterPlaymodeController.cs
@@ -1,6 +1,6 @@
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEngine;
 
 /// <summary>
 /// This class lets me enter play mode from whereever i want and still load up the login screen. When entering the game not via the login screen, stuff does not work properly as there is no user data created.
@@ -8,12 +8,19 @@ using UnityEngine;
 [InitializeOnLoad]
 public class EnterPlaymodeController
 {
+    #region Settings
+
     private const string DefaultScenePath = "Assets/Scenes/LoginScreen.unity";
+
+    private const bool AutoLoginAsAdmin = true;
+
+    #endregion
 
     static EnterPlaymodeController() => EditorApplication.playModeStateChanged += LoadDefaultScene;
 
     private static void LoadDefaultScene(PlayModeStateChange state)
     {
+        // Switching to login screen when entering play mode
         if (state == PlayModeStateChange.ExitingEditMode)
         {
             if (EditorSceneManager.GetActiveScene().path.Equals(DefaultScenePath)) return;
@@ -32,11 +39,21 @@ public class EnterPlaymodeController
             EditorApplication.isPlaying = false;
         }
 
-        if (state == PlayModeStateChange.EnteredEditMode)
+        // Returning to previous scene after deactivating play mode
+        else if (state == PlayModeStateChange.EnteredEditMode)
         {
             if (!PlayerPrefs.HasKey("PreviousScenePath")) return;
 
             EditorSceneManager.OpenScene(PlayerPrefs.GetString("PreviousScenePath"));
+        }
+
+        // Auto login after going into play mode
+        else if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+#pragma warning disable CS0162 // Unreachable code detected
+            if (!AutoLoginAsAdmin) return;
+#pragma warning restore CS0162 // Unreachable code detected
+            Object.FindAnyObjectByType<LoginManager>().TrySubmitLogin("admin", "t8734qzp920ßtvhrtbui23op");
         }
     }
 }
