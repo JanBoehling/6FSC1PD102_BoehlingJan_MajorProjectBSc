85194c5a099b4b5220048975797f962f68ef9fe7
JanBoehling <122728039+JanBoehling@users.noreply.github.com>
2025-07-23 20:49:53 +0200

    Added layout-corrected version of the Transkription video and added editor tool to force play specific video clips in video assignment

--- Assets/Scripts/Assignments/Video/Controller/VideoAssignmentController.cs
+++ Assets/Scripts/Assignments/Video/Controller/VideoAssignmentController.cs
@@ -16,8 +16,7 @@ public class VideoAssignmentController : MonoBehaviour
     [SerializeField] private RectTransform _quitMessageContainer;
     [SerializeField] private RectTransform _videoControlsContainer;
     [SerializeField] private Button _continueButton;
-    [Space]
-    [SerializeField] private VideoAssignment _debugVideoAssignment;
+    [field: SerializeField] public VideoClip DebugVideoClip { get; private set; }
     [Space]
     [SerializeField] private float _timer;
     
@@ -25,7 +24,11 @@ public class VideoAssignmentController : MonoBehaviour
     private AudioSource _audioPlayer;
     private ParticleSystem _confettiCanon;
 
+    private Coroutine _timerCoroutine;
+
     private uint _assignmentID;
+
+    private bool _hasLoadedOnce = false;
 
     private void Awake()
     {
@@ -37,26 +40,59 @@ public class VideoAssignmentController : MonoBehaviour
 
     private void Start()
     {
+        Init();
+    }
+
+    public void Init()
+    {
         _assignmentID = RuntimeDataHolder.CurrentMilestone.Assignments[0];
         var assignmentData = UnitAndAssignmentManager.Instance.GetAssignmentByID(_assignmentID) as VideoAssignment;
 
-        if (assignmentData == null)
-        {
-            assignmentData = _debugVideoAssignment;
-            Debug.LogWarning("Could not fetch video assignment data. Using debug video assignment instead.");
-        }
-
         StartVideo(assignmentData.VideoURL);
 
-        _videoPlayer.prepareCompleted += PlayVideo;
+        if (!_hasLoadedOnce) _videoPlayer.prepareCompleted += PlayVideo;
+        _hasLoadedOnce = true;
     }
 
+    /// <summary>
+    /// Debug version of Init that is used to directly load a specific video clip
+    /// </summary>
+    /// <param name="clip">The video clip that should be force played</param>
+    public void Init(VideoClip clip)
+    {
+        _assignmentID = RuntimeDataHolder.CurrentMilestone.Assignments[0];
+        var assignmentData = UnitAndAssignmentManager.Instance.GetAssignmentByID(_assignmentID) as VideoAssignment;
+
+        StartVideo(clip);
+
+        if (!_hasLoadedOnce) _videoPlayer.prepareCompleted += PlayVideo;
+        _hasLoadedOnce = true;
+    }
+
+    /// <summary>
+    /// Loads the video clip from the given URL and starts the video player
+    /// </summary>
+    /// <param name="videoURL">The URL of the video to be played</param>
     public void StartVideo(string videoURL)
     {
         _videoPlayer.EnableAudioTrack(0, true);
         _videoPlayer.SetTargetAudioSource(0, _audioPlayer);
 
         _videoPlayer.url = videoURL;
+
+        _videoPlayer.Prepare();
+    }
+
+    /// <summary>
+    /// Starts the video player with the given video clip
+    /// </summary>
+    /// <param name="videoClip">The video clip that should be played</param>
+    public void StartVideo(VideoClip videoClip)
+    {
+        _videoPlayer.EnableAudioTrack(0, true);
+        _videoPlayer.SetTargetAudioSource(0, _audioPlayer);
+
+        _videoPlayer.clip = videoClip;
 
         _videoPlayer.Prepare();
     }
@@ -100,7 +136,12 @@ public class VideoAssignmentController : MonoBehaviour
         src.Play();
         _audioPlayer.Play();
 
-        StartCoroutine(VideoWatchtimeWatcherCO());
+        if (_timerCoroutine != null)
+        {
+            StopCoroutine(_timerCoroutine);
+            _timer = 0f;
+        }
+        _timerCoroutine = StartCoroutine(VideoWatchtimeWatcherCO());
     }
 
     private IEnumerator VideoWatchtimeWatcherCO()
@@ -146,6 +187,10 @@ public class VideoAssignmentControllerEditor : Editor
         // Displays the current watchtime and the duration of the video
         EditorGUILayout.Space();
         EditorGUILayout.HelpBox($"{_assignmentController.CurrentWatchtime:F2} / {_assignmentController.VideoDuration:F2}", MessageType.None, true);
+
+        EditorGUILayout.Separator();
+
+        if (GUILayout.Button("Force Play Video")) _assignmentController.Init(_assignmentController.DebugVideoClip);
     }
 
     public override bool RequiresConstantRepaint() => true;
