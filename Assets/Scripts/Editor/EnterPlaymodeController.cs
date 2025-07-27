186e34904303ba78a97e808ebc0866f4877e6d29
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-27 16:35:02 +0200

    Added login button to editor play mode controller script

--- Assets/Scripts/Editor/EnterPlaymodeController.cs
+++ Assets/Scripts/Editor/EnterPlaymodeController.cs
@@ -13,55 +13,96 @@ public class EnterPlaymodeController
     private const string DefaultScenePath = "Assets/Scenes/LoginScreen.unity";
 
     private const string AutoLoginMennuItem = "Biolexica/Auto Login";
+    private const string LoginAsAdminMennuItem = "Biolexica/Login as Admin";
 
     #endregion
 
     private static bool AutoLoginAsAdmin => Menu.GetChecked(AutoLoginMennuItem);
 
-    static EnterPlaymodeController() => EditorApplication.playModeStateChanged += LoadDefaultScene;
+    private static readonly System.Action _loginAsAdmin = () => Object.FindAnyObjectByType<LoginManager>().TrySubmitLogin("admin", "t8734qzp920ßtvhrtbui23op");
 
+    static EnterPlaymodeController() => EditorApplication.playModeStateChanged += OnModeSwitch;
+
+    /// <summary>
+    /// When active, automatically logs in as admin when entering play mode
+    /// </summary>
     [MenuItem(AutoLoginMennuItem)]
-    public static void ToggleAutoLoginMenu()
+    public static void ToggleAutoLoginMenu() => Menu.SetChecked(AutoLoginMennuItem, !AutoLoginAsAdmin);
+
+    /// <summary>
+    /// Validates the menu item to log the user in as admin. The button may only be active if the login scene is active and the editor is in play mode
+    /// </summary>
+    /// <returns></returns>
+    [MenuItem(LoginAsAdminMennuItem, true)]
+    public static bool ValidateLoginMenu() => EditorSceneManager.GetActiveScene().buildIndex == 0 && Application.isPlaying;
+
+    /// <summary>
+    /// When in login screen and in play mode, this menu item logs the user in as admin
+    /// </summary>
+    [MenuItem(LoginAsAdminMennuItem)]
+    public static void LoginMenu() => _loginAsAdmin.Invoke();
+
+    /// <summary>
+    /// Executes specific behaviour when switching play mode states
+    /// </summary>
+    /// <param name="state">The current play mode state</param>
+    private static void OnModeSwitch(PlayModeStateChange state)
     {
-        Menu.SetChecked(AutoLoginMennuItem, !AutoLoginAsAdmin);
+        switch (state)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+                ReturnToPreviousScene();
+                break;
+            case PlayModeStateChange.ExitingEditMode:
+                SwitchToLoginScreen();
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                AutoLogin();
+                break;
+            case PlayModeStateChange.ExitingPlayMode:
+                break;
+            default:
+                break;
+        }
     }
 
-    private static void LoadDefaultScene(PlayModeStateChange state)
+    /// <summary>
+    /// Returning to previous scene after deactivating play mode
+    /// </summary>
+    private static void ReturnToPreviousScene()
     {
-        // Switching to login screen when entering play mode
-        if (state == PlayModeStateChange.ExitingEditMode)
+        if (!PlayerPrefs.HasKey("PreviousScenePath")) return;
+
+        EditorSceneManager.OpenScene(PlayerPrefs.GetString("PreviousScenePath"));
+    }
+
+    /// <summary>
+    /// Switching to login screen when entering play mode
+    /// </summary>
+    private static void SwitchToLoginScreen()
+    {
+        if (EditorSceneManager.GetActiveScene().path.Equals(DefaultScenePath)) return;
+
+        PlayerPrefs.SetString("PreviousScenePath", EditorSceneManager.GetActiveScene().path);
+        PlayerPrefs.Save();
+
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+        EditorApplication.delayCall += () =>
         {
-            if (EditorSceneManager.GetActiveScene().path.Equals(DefaultScenePath)) return;
+            EditorSceneManager.OpenScene(DefaultScenePath);
+            EditorApplication.isPlaying = true;
+        };
 
-            PlayerPrefs.SetString("PreviousScenePath", EditorSceneManager.GetActiveScene().path);
-            PlayerPrefs.Save();
+        EditorApplication.isPlaying = false;
+    }
 
-            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-
-            EditorApplication.delayCall += () =>
-            {
-                EditorSceneManager.OpenScene(DefaultScenePath);
-                EditorApplication.isPlaying = true;
-            };
-
-            EditorApplication.isPlaying = false;
-        }
-
-        // Returning to previous scene after deactivating play mode
-        else if (state == PlayModeStateChange.EnteredEditMode)
-        {
-            if (!PlayerPrefs.HasKey("PreviousScenePath")) return;
-
-            EditorSceneManager.OpenScene(PlayerPrefs.GetString("PreviousScenePath"));
-        }
-
-        // Auto login after going into play mode
-        else if (state == PlayModeStateChange.EnteredPlayMode)
-        {
-#pragma warning disable CS0162 // Unreachable code detected
-            if (!AutoLoginAsAdmin) return;
-#pragma warning restore CS0162 // Unreachable code detected
-            Object.FindAnyObjectByType<LoginManager>().TrySubmitLogin("admin", "t8734qzp920ßtvhrtbui23op");
-        }
+    /// <summary>
+    /// Auto login after going into play mode
+    /// </summary>
+    private static void AutoLogin()
+    {
+        if (!AutoLoginAsAdmin) return;
+        _loginAsAdmin.Invoke();
     }
 }
