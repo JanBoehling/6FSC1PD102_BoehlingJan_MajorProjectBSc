c3f36f96af05e75b3be7a5b7aa3b07c98bd320eb
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-27 16:39:09 +0200

    Removed unneccessary events

--- Assets/Scripts/MenuElements/FeedbackWindowController.cs
+++ Assets/Scripts/MenuElements/FeedbackWindowController.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
-using UnityEngine.Events;
 using System.Runtime.InteropServices;
 
 public class FeedbackWindowController : MonoBehaviour
 {
     [SerializeField] private string _feedbackFormURL;
-    [Space]
-    [SerializeField] private UnityEvent _onEnable;
-    [SerializeField] private UnityEvent _onDisable;
-
-    private void OnEnable() => _onEnable?.Invoke();
-
-    private void OnDisable() => _onDisable?.Invoke();
 
     public void OpenFeedbackForm()
     {
