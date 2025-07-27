5f9456683767f64838b884c55f1bfdd49746689d
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-27 15:29:36 +0200

    Added feedback button with raw button sprite

--- Assets/Scripts/MenuElements/FeedbackWindowController.cs
+++ Assets/Scripts/MenuElements/FeedbackWindowController.cs
@@ -1,60 +1,27 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
 using System.Runtime.InteropServices;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class FeedbackWindowController : MonoBehaviour
 {
     [SerializeField] private string _feedbackFormURL;
+    [Space]
+    [SerializeField] private UnityEvent _onEnable;
+    [SerializeField] private UnityEvent _onDisable;
 
-    private Image _image;
-    private GameObject _container;
+    private void OnEnable() => _onEnable?.Invoke();
 
-    public void Awake()
-    {
-        _image = GetComponent<Image>();
-        _container = transform.GetChild(0).gameObject;
-    }
-
-    public void ToggleVisibility()
-    {
-        _image.enabled = !_image.enabled;
-        _container.SetActive(!_container.activeSelf);
-    }
+    private void OnDisable() => _onDisable?.Invoke();
 
     public void OpenFeedbackForm()
     {
 #if !UNITY_EDITOR
 		OpenWindow(_feedbackFormURL);
+#else
+        Application.OpenURL(_feedbackFormURL);
 #endif
     }
 
     [DllImport("__Internal")]
     private static extern void OpenWindow(string url);
 }
-
-#if UNITY_EDITOR
-[CustomEditor(typeof(FeedbackWindowController))]
-public class FeedbackWindowControllerEditor : Editor
-{
-    private FeedbackWindowController _controller;
-
-    private void OnEnable()
-    {
-        _controller = (FeedbackWindowController)target;
-    }
-
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        if (!GUILayout.Button("Toggle Visibility")) return;
-
-        _controller.Awake();
-        _controller.ToggleVisibility();
-    }
-}
-#endif
