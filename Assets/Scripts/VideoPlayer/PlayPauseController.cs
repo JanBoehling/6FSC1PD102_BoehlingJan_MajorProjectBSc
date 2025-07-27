91bfabebedebfeace0716ba97ad8763504191418
Jan Böhling <janrasmus03.boehling@t-online.de>
2025-07-27 19:32:00 +0200

    Pause button will now disappear after 1 second and when resuming a play icon will appear

--- Assets/Scripts/VideoPlayer/PlayPauseController.cs
+++ Assets/Scripts/VideoPlayer/PlayPauseController.cs
@@ -1,23 +1,44 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class PlayPauseController : VideoControlsBase, IPointerClickHandler
 {
-    [SerializeField] private UnityEvent _onPlayEvent;
-    [SerializeField] private UnityEvent _onPauseEvent;
+    [SerializeField] private Sprite _playIcon;
+    [SerializeField] private Sprite _pauseIcon;
+    [SerializeField] private Image _playPauseImage;
+    [Space]
+    [SerializeField] private float _animationDuration = 1f;
+
+    private Coroutine _playPauseAnimation;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (_videoPlayer.isPlaying)
         {
             _videoPlayer.Pause();
-            _onPauseEvent?.Invoke();
+
+            if (_playPauseAnimation != null) StopCoroutine(_playPauseAnimation);
+            _playPauseAnimation = StartCoroutine(AnimatePlayPauseCO(_pauseIcon));
         }
         else
         {
             _videoPlayer.Play();
-            _onPlayEvent?.Invoke();
+
+            if (_playPauseAnimation != null) StopCoroutine(_playPauseAnimation);
+            _playPauseAnimation = StartCoroutine(AnimatePlayPauseCO(_playIcon));
         }
+    }
+
+    private IEnumerator AnimatePlayPauseCO(Sprite playPauseSprite)
+    {
+        _playPauseImage.sprite = playPauseSprite;
+
+        _playPauseImage.gameObject.SetActive(true);
+        yield return new WaitForSeconds(_animationDuration);
+        _playPauseImage.gameObject.SetActive(false);
+
+        _playPauseAnimation = null;
     }
 }
