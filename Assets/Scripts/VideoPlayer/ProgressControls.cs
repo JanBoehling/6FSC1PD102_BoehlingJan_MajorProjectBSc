98ed004ebbcda3686867e64cf54d38d4317567a1
JanBoehling <122728039+JanBoehling@users.noreply.github.com>
2025-07-30 23:55:09 +0200

    Fixed null ref exception in video assignment scene

--- Assets/Scripts/VideoPlayer/ProgressControls.cs
+++ Assets/Scripts/VideoPlayer/ProgressControls.cs
@@ -1,14 +1,7 @@
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class ProgressControls : VideoControlsBase, IPointerDownHandler
 {
-    protected override void Awake()
-    {
-        base.Awake();
-        ProgressBar = GetComponent<Image>();
-    }
-
     private void Update() => ProgressBar.fillAmount = VideoProgress;
 
     public void OnPointerDown(PointerEventData eventData) => Wind(eventData.position);
